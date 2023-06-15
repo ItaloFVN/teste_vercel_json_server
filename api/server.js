f5d0c0793fa65f6f336188c042c448e9061820b9
ItaloFVN <ifvnasc@gmail.com>
2023-06-15 14:19:46 -0300

    retorno pra versão normal

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const fs = require('fs')
-const db = JSON.parse(fs.readFileSync('https://nutrischedule.vercel.app/database/db.json'))
+const db = JSON.parse(fs.readFileSync('database/db.json'))
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(db)
